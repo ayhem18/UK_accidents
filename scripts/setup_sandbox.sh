944b4cf7e9a69728af563dc0bf25d4cbd900bd2f
root <root@sandbox-hdp.hortonworks.com>
2023-05-07 15:08:13 +0000

    changes

--- scripts/setup_sandbox.sh
+++ scripts/setup_sandbox.sh
@@ -7,10 +7,10 @@ echo "export TERM=xterm vim" >> /root/.bashrc
 source /root/.bashrc
 
 ## Correct postgres configuration
-sed -i '1s/^/local all all trust\n/' /var/lib/pgsql/9.6/data/pg_hba.conf
-sed -i '1s/^/host all all 0.0.0.0\/0 trust\n/' /var/lib/pgsql/9.6/data/pg_hba.conf
+sed -i '1s/^/local all all trust\n/' /var/lib/pgsql/data/pg_hba.conf
+sed -i '1s/^/host all all 0.0.0.0\/0 trust\n/' /var/lib/pgsql/data/pg_hba.conf
 
-sudo systemctl restart postgresql-9.6
+sudo systemctl restart postgresql
 
 ## Install Python 3
 sudo yum -y groupinstall "Development Tools"
@@ -18,8 +18,8 @@ sudo yum -y install openssl-devel bzip2-devel libffi-devel xz-devel
 
 sudo yum -y install wget
 wget https://www.python.org/ftp/python/3.8.16/Python-3.8.16.tgz -P ~
-tar xvf ~/Python-3.8.16.tgz
-cd ~/Python-3.8*/
+tar xvf ~/Python-3.8.16.tgz -C ~
+cd ~/Python-3.8.16/
 
 ./configure --enable-optimizations
 sudo make altinstall
@@ -27,7 +27,7 @@ sudo make altinstall
 # Create symlinks for convenience of use
 #yes | rm /usr/bin/python
 ln -s /usr/local/bin/python3.8 /usr/bin/python3
-#mv /usr/bin/pip3 /usr/bin/pip_bak
+mv /usr/bin/pip3 /usr/bin/pip_bak
 ln -s /usr/local/bin/pip3.8 /usr/bin/pip3
 
 # Sqoop
