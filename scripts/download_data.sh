51b8e0326d8ab7e2a5dae4e4b686354be946e7e7
root <root@sandbox-hdp.hortonworks.com>
2023-05-07 14:57:06 +0000

    changes

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -20,7 +20,7 @@ FILENAME='data/casualties.csv'
 
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=$FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -O $FILENAME && rm -rf /tmp/cookies.txt
 
-FILEID='1hM_hyTdLx3Xv2E5cLTdWVAVAeeqMyqO'
+FILEID='1hM_hyTdLx3Xv2E5cLTdWVAVAeeqMyqOF'
 FILENAME='data/Road-Accident-Safety-Data-Guide.xls'
 
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=$FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -O $FILENAME && rm -rf /tmp/cookies.txt
